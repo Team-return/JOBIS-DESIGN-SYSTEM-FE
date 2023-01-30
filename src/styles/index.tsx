ce31c6f2303ab4705261138efe9ba44902cc8eea
jikwan <jikwan0327@gmail.com>
2023-01-30 15:50:29 +0900

    fix :: childern 타입 수정

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from './theme';
 
-export const StyledProvider = ({ children }) => {
+export const StyledProvider = ({ children }: any) => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
