cca764d093e50cd236afaa468697ad16ecc7eab1
jikwan <jikwan0327@gmail.com>
2023-01-30 17:31:29 +0900

    feat :: CheckBox 추가

---
+++ src/components/checkbox/CheckBox.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { marginCssType, marginToCss } from '../../utils/distance';
+import * as C from '../../styles/theme/color';
+import * as F from '../../styles/theme/font';
+
+interface CheckBoxProps extends marginCssType {
+  //   label?: string;
+  className?: string;
+  disabled: boolean;
+  checked?: boolean;
+  onClick?: () => void;
+}
+
+export const CheckBox = ({
+  className,
+  disabled = false,
+  checked,
+  onClick,
+  margin,
+}: CheckBoxProps) => {
+  return (
+    <_CheckBoxWrapper>
+      <_Wrapper
+        className={className}
+        onClick={onClick}
+        margin={margin}
+        disabled={disabled}
+        type="checkbox"
+        checked={checked}
+      />
+    </_CheckBoxWrapper>
+  );
+};
+
+const _CheckBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const _Wrapper = styled.input<CheckBoxProps>`
+  width: 16px;
+  height: 16px;
+  ${({ margin }) => marginToCss({ margin })};
+  accent-color: #0087ff;
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'auto')};
+`;
