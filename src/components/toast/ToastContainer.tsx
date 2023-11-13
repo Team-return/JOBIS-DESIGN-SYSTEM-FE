51ed36e4c3eccf19e19fb7fee56e0380bd488031
jikwan <jikwan0327@gmail.com>
2023-11-13 22:49:56 +0900

    fix: Toast 위치 버그 수정

--- src/components/toast/ToastContainer.tsx
+++ src/components/toast/ToastContainer.tsx
@@ -18,8 +18,9 @@ export const ToastContainer = () => {
 };
 
 const _Container = styled.div`
-  position: absolute;
+  position: fixed;
   top: 30px;
   right: 30px;
   gap: 20px;
+  z-index: 100000;
 `;
